92f2831b2c800dbc632681348730c4ba67ffd4cf
Jonathan Carifio <carifio24@gmail.com>
2026-06-26 15:59:50 -0400

    Use a custom subclass of BaseColormap rather than hackily modifying the Vispy Colormap class.

--- glue_vispy_viewers/volume/colors.py
+++ glue_vispy_viewers/volume/colors.py
@@ -1,6 +1,8 @@
+import numpy as np
 from glue.config import AsinhStretch, LinearStretch, LogStretch, SqrtStretch, DictRegistry
 from matplotlib.colors import ListedColormap
-from vispy.color import BaseColormap, Colormap
+from vispy.color import BaseColormap, ColorArray
+from vispy.color.colormap import LUT_len
 from uuid import uuid4
 
 
@@ -24,14 +26,39 @@ def glsl_for_stretch(stretch, parameter="t"):
     return template.format(stretch=stretch, parameter=parameter)
 
 
-class CustomColormap(Colormap):
+# This is based on `vispy.color.colormap.Colormap`
+# In a similar vein to how we needed to create our own MultiVolumeVisual class,
+# the primary issue with the VisPy Colormap class is that the colormap shader
+# creates a particular name for the sampler uniform, but we need a separate
+# sampler for each layer
+class CustomColormap(BaseColormap):
 
-    def __init__(self, colors, controls=None, interpolation="linear"):
-        super().__init__(colors, controls=controls, interpolation=interpolation)
+    def __init__(self, colors, controls=None, *,
+                 bad_color=None, low_color=None, high_color=None):
+
+        ncontrols = len(colors)
+        if controls is None:
+            controls = np.linspace(0.0, 1.0, ncontrols)
+        assert len(controls) == ncontrols
+        self._controls = np.array(controls, dtype=np.float32)
+
+        c_rgba = ColorArray(colors).rgba
+        self.texture_map_data = np.zeros((LUT_len, 1, 4), dtype=np.float32)
+        self.texture_map_data[:, 0, 0] = np.interp(x, controls, c_rgba[:, 0])
+        self.texture_map_data[:, 0, 1] = np.interp(x, controls, c_rgba[:, 1])
+        self.texture_map_data[:, 0, 2] = np.interp(x, controls, c_rgba[:, 2])
+        self.texture_map_data[:, 0, 3] = np.interp(x, controls, c_rgba[:, 3])
 
         self.uuid = uuid4().hex
         self.texture_name = f"texture2D_LUT_{self.uuid}"
-        self.glsl_map = self.glsl_map.replace("texture2D_LUT", self.texture_name)
+        self.glsl_map = f"""
+        uniform sample2D {self.texture_name};
+        vec4 colormap(float t) {{
+            return texture2D({self.texture_name}, vec2(0.0, clamp(t, 0.0, 1.0)));
+        }}
+        """
+        super().__init__(colors, bad_color=bad_color,
+                         high_color=high_color, low_color=low_color)
 
 
 def get_translucent_cmap(r, g, b, stretch):
@@ -60,4 +87,4 @@ def get_mpl_cmap(cmap, stretch):
         ts = stretch([index / (n_colors - 1) for index in range(n_colors)])
         colors = [[*cmap(t)[:3], t] for t in ts]
 
-    return CustomColormap(colors=colors, controls=ts, interpolation="linear")
+    return CustomColormap(colors=colors, controls=ts)
